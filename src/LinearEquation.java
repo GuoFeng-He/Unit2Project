ec3f94e42f252a9430fa9f4104e0a828090e0d4d
BT_2N2_27 <sample@bths.edu>
2023-10-16 14:06:41 -0400

    recommit (just in case code didn't push)

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    private double slope(){
+        return (double) (y2 - y1) / (x2 - x1);
+    }
+
+
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class LinearEquationLogic {
+import java.util.Scanner;
+public class LinearEquationLogic {
+    Scanner scan;
+
+    public LinearEquationLogic(){
+        scan = new Scanner(System.in);
+    }
+
+    public void start(){
+
+    }
+
+    private void getData(){
+        System.out.print("Enter first coordinate point: ");
+        String point1 = scan.nextLine();
+        System.out.print("Enter second coordinate point: ");
+        String point2 = scan.nextLine();
+
+        int x1 = parseX(point1);
+        int y1 = parseY(point1);
+        int x2 = parseX(point2);
+        int y2 = parseY(point2);
+        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
+    }
+
+    private int parseX(String point){
+        int commaIdx = point.indexOf(",");
+        return Integer.parseInt(point.substring(1,commaIdx));
+    }
+
+    private int parseY(String point){
+        int commaIdx = point.indexOf(",");
+        return Integer.parseInt(point.substring(commaIdx + 2, point.length() - 1));
+    }
 }
