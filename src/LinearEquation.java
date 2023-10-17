8e9944d1ab90dde79c1174447736ed7130c4eb01
BT_2N2_27 <sample@bths.edu>
2023-10-17 14:05:31 -0400

    added some more methods to LinearEquation.java

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -12,8 +12,15 @@ public class LinearEquation {
     }
 
     private double slope(){
-        return (double) (y2 - y1) / (x2 - x1);
+        return Math.round(((double) y2 - y1) / (x2 - x1));
     }
 
+    private double distance(int x1, int y1, int x2, int y2){
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    private String equation(){
+        return "y = " + slope() + "x + ";
+    }
 
 }
