7e8a109e60602e43b86c187f13e61940e08571a4
Guo Feng He <guofengh@nycstudents.net>
2023-10-30 19:59:47 -0400

    finished program (along with extra credit) ready for feedback

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -11,19 +11,61 @@ public class LinearEquation {
         this.y2 = y2;
     }
 
-    public double slope(){
-        return roundToHundredth(((double) y2 - y1) / (x2 - x1));
+    private double deltaX(){
+        return (double) x2 - x1;
+    }
+    private double deltaY(){
+        return (double) y2 - y1;
+    }
+
+    private double slope(){
+        return roundToHundredth((deltaY() / deltaX()));
+    }
+
+    /* 3rd extra credit criteria (part 1): all slopes returned from this method are positive so the negative sign can
+    get added before the slope in equation()*/
+    // 4th extra credit criteria: all slopes returned are positive so a double negative will still make a positive slope
+    private String fractionalSlope(){
+        // 1st extra credit criteria: rounds slope to the nearest whole number if the fraction reduces to a whole number
+        if (deltaY() % deltaX() == 0){
+            return "" + (int) Math.abs((deltaY() / deltaX()));
+        }
+        return (int) Math.abs(deltaY()) + "/" + (int) Math.abs(deltaX());
     }
 
     private double distance(int x1, int y1, int x2, int y2){
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return roundToHundredth(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
     }
 
     private String equation(){
+        String baseEquation = "y = ";
+        double yIntercept = yIntercept();
+        String slopeSign = "";
+        String slope = fractionalSlope();
+
         if (y2 == y1){
-            return "y=" + y1;
+            return baseEquation + y1;
         }
-        return "y=" + slope() + "x+" + yIntercept();
+        // 3rd extra credit criteria (part 2): this makes sure that the negative sign only gets placed before the slope
+        if (slope() < 0){
+            slopeSign = "-";
+        }
+        // 2nd extra credit criteria: 1 gets omitted if the slope is -1 or 1
+        if (slope() == 1 || slope() == -1){
+            slope = "";
+        }
+        baseEquation += slopeSign + slope + "x";
+        /* 5th extra credit criteria: omits y-intercept if it's equal to 0 (y-intercept will not get concatenated if
+        it's equal 0). If both the y intercept and slope are equal to 0, returns y = 0*/
+        if (yIntercept == 0 && slope() == 0){
+            return "y = 0";
+        } else if (yIntercept < 0){                         // 6th extra credit criteria: only the negative sign will
+            baseEquation += " - " + Math.abs(yIntercept);   // get printed if y-intercept is negative
+        } else if (yIntercept > 0){
+            baseEquation += " + " + yIntercept;
+        }
+        // returns the final equation!
+        return baseEquation;
     }
 
     public String coordinateForX(double x){
@@ -40,7 +82,11 @@ public class LinearEquation {
     }
 
     public String lineInfo(){
-        return "The two";
+        return "\n\nThe two points are: (" + x1 + ", " + y1 + ")" + " and (" + x2 + ", " + y2 + ")" + "\n" +
+                "The equation of the line between these points is: " + equation() + "\n" +
+                "The slope of this line is: " + slope() + "\n" +
+                "The y-intercept of this line is: " + yIntercept() + "\n" +
+                "The distance between these points is " + distance(x1, y1, x2, y2);
     }
 
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -17,12 +17,34 @@ public class LinearEquationLogic {
         String point2 = scan.nextLine();
 
         int x1 = parseX(point1);
-        int y1 = parseY(point1);
         int x2 = parseX(point2);
+
+        // undefined slope check (freestyle)
+        while (x1 == x2){
+            System.out.println("The points you entered results in a line with an undefined slope");
+            System.out.println("Please try again\n\n");
+            System.out.print("Enter first coordinate point: ");
+            point1 = scan.nextLine();
+            System.out.print("Enter second coordinate point: ");
+            point2 = scan.nextLine();
+
+            x1 = parseX(point1);
+            x2 = parseX(point2);
+        }
+
+        int y1 = parseY(point1);
         int y2 = parseY(point2);
         LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
-        System.out.println(equation.slope());
-        System.out.println(equation.yIntercept());
+        System.out.println(equation.lineInfo());
+
+        String yesNo = "";
+        while (!yesNo.toLowerCase().equals("no")) {
+            System.out.print("\nEnter a x value: ");
+            double xInput = scan.nextDouble();
+            System.out.println("Your corresponding coordinate is: " + equation.coordinateForX(xInput));
+            System.out.print("Again? (yes/no): ");
+            yesNo = scan.nextLine();
+        }
     }
 
     private int parseX(String point){
@@ -34,4 +56,5 @@ public class LinearEquationLogic {
         int commaIdx = point.indexOf(",");
         return Integer.parseInt(point.substring(commaIdx + 2, point.length() - 1));
     }
+
 }
